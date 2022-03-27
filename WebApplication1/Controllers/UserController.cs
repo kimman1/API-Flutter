b1f6be500a69373e1553fda20cbd9cc798cbdeb2
KimMan <KimMan@DESKTOP-T37MUQQ>
2022-03-28 01:01:10 +0700

    first commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using GoogleApi.Entities.Places.Search.Text.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : ApiController
+    {
+        QLThuChiEntities db = new QLThuChiEntities();
+        public IEnumerable<Login> GetUser()
+        {
+            List<Login> listResult = db.Logins.ToList();
+            if (listResult.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Logins.ToList();
+            }
+            
+        }
+    }
+}
